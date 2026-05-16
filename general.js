75aa0350fee454b317d588e0828c7ad47f110b14
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 15:33:37 +0530

    Update general.js

--- general.js
+++ general.js
@@ -1,21 +1,42 @@
 const axios = require('axios');
 
 async function getAllBooks() {
-  const response = await axios.get('http://localhost:5000/');
-  console.log(response.data);
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getBookByISBN(isbn) {
-  const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
-  console.log(response.data);
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getBookByAuthor(author) {
-  const response = await axios.get(`http://localhost:5000/author/${author}`);
-  console.log(response.data);
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getBookByTitle(title) {
-  const response = await axios.get(`http://localhost:5000/title/${title}`);
-  console.log(response.data);
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+getAllBooks();
+getBookByISBN(1);
+getBookByAuthor("Chinua Achebe");
+getBookByTitle("Things Fall Apart");
