3d26b972447b26ae81e09a854484cb468929f724
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 15:22:35 +0530

    Update general.js

--- general.js
+++ general.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
 
+async function getAllBooks() {
+  const response = await axios.get('http://localhost:5000/');
+  console.log(response.data);
+}
+
+async function getBookByISBN(isbn) {
+  const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+  console.log(response.data);
+}
+
+async function getBookByAuthor(author) {
+  const response = await axios.get(`http://localhost:5000/author/${author}`);
+  console.log(response.data);
+}
+
+async function getBookByTitle(title) {
+  const response = await axios.get(`http://localhost:5000/title/${title}`);
+  console.log(response.data);
+}
